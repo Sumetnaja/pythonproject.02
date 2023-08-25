c1e0093486de9bba58f87625c12e6cb977d55943
Sumetnaja <Sumet471@gmail.com>
2023-08-25 12:32:37 +0700

    code v.2 :pythonproject02

---
+++ test05.py
@@ -0,0 +1,12 @@
+# one by one
+#small to big
+print("Bangna" > "Bangbon") #true
+print("a"  > "A")           #True
+print("SAU" > "sau")        #false
+
+#Logical Operator
+#and   or   not
+#-------------------------------------->
+#* / % //, + - , < > , = == != <= >= , not , and ,or
+print(7+3*2-5)
+
